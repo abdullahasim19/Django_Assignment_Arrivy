ec84bc33cf02133e096173c30dbbad72d8dc074e
abdullah <abdullah.asim@arrivy.com>
2023-08-04 17:16:40 +0500

    Missing field validation error and other issues fixed

--- accounts/views.py
+++ accounts/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-from django.http import HttpResponse,HttpResponseRedirect
+from django.http import HttpResponse, HttpResponseRedirect
 
 from django.contrib.auth.hashers import make_password
 from django.http import JsonResponse
@@ -8,6 +8,8 @@ from django.contrib.auth.models import User
 import re
 from django.contrib.auth import authenticate, login, logout
 from django.urls import reverse
+
+
 # Create your views here.
 
 def account_index(request):
@@ -29,19 +31,32 @@ def register_user(request):
         except json.JSONDecodeError:
             return JsonResponse({'error': 'Invalid JSON data'}, status=400)
 
+        field_values = ['username', 'password1', 'password2', 'first_name', 'last_name', 'email']
+        missing_fields = []
+        for val in field_values:
+            if val not in data.keys():
+                missing_fields.append(val)
+
+        if len(missing_fields) > 0:
+            miss = ', '.join(missing_fields)
+            return HttpResponse(f'Following are missing in request body: {miss}', status=400)
+        # return HttpResponse('Testing')
+
         username = data.get('username')
+        password1 = data.get('password1')
+        password2 = data.get('password2')
+        first_name = data.get('first_name')
+        last_name = data.get('last_name')
+        email = data.get('email')
+
+        if not username or not password1 or not password2:
+            return JsonResponse({'error': 'Username or Password is missing'}, status=400)
         try:
             checkuser = User.objects.get(username=username)
         except:
             checkuser = None
         if checkuser is not None:
             return JsonResponse({'error': 'A user with that username already exists'}, status=400)
-
-        password1 = data.get('password1')
-        password2 = data.get('password2')
-
-        if not username or not password1 or not password2:
-            return JsonResponse({'error': 'Username or Password is missing'}, status=400)
 
         if password1 != password2:
             return JsonResponse({"error": "The two password fields didn't match"}, status=400)
@@ -50,62 +65,74 @@ def register_user(request):
             return JsonResponse({"error": "This password is too short. It must contain at least 8 characters"}
                                 , status=400)
 
-        first_name = data.get('first_name')
-        last_name = data.get('last_name')
-        email = data.get('email')
         if email:
             if not is_valid_email(email):
                 return JsonResponse({'error': 'Enter a valid email address'}, status=400)
 
-        #user = User.objects.create(username=username, password=password1, email=email, first_name=first_name
-                                #  , last_name=last_name
-                                 #  )
-        user = User(username=username, email=email,first_name=first_name,last_name=last_name)
+        # user = User.objects.create(username=username, password=password1, email=email, first_name=first_name
+        #  , last_name=last_name
+        #  )
+        user = User(username=username, email=email, first_name=first_name, last_name=last_name)
         user.set_password(password1)
         user.save()
 
         return JsonResponse({'message': 'User created successfully', }, status=201)
     else:
-        return HttpResponse("Invalid Request Type",status=400)
+        return HttpResponse("Invalid Request Type", status=400)
+
 
 def login_user(request):
-    #print('dataa', request)
-    if request.method=='GET':
+    # print('dataa', request)
+    if request.method == 'GET':
         if request.user.is_authenticated:
             return HttpResponseRedirect(reverse('profile'))
         else:
             return HttpResponse('No user is logged in!')
-    elif request.method=='POST':
+    elif request.method == 'POST':
         try:
             data = json.loads(request.body)
         except json.JSONDecodeError:
             return JsonResponse({'error': 'Invalid JSON data'}, status=400)
-        username=data.get('username')
-        password=data.get('password')
+
+        field_values = ['username', 'password']
+        missing_fields = []
+        for val in field_values:
+            if val not in data.keys():
+                missing_fields.append(val)
+
+        if len(missing_fields) > 0:
+            miss = ', '.join(missing_fields)
+            return HttpResponse(f'Following are missing in request body: {miss}', status=400)
+
+        username = data.get('username')
+        password = data.get('password')
+        if not username or not password:
+            return JsonResponse({'error': 'Username or Password is missing'}, status=400)
+
         if request.user.is_authenticated:
-            #request.session['message']='Already logged in!'
+            # request.session['message']='Already logged in!'
             return HttpResponseRedirect(reverse('profile'))
         user = authenticate(request, username=username, password=password)
 
         if user:
-            login(request,user)
-            #return HttpResponse('Login Done')
-            #request.session['message'] = 'Login Successful!!'
+            login(request, user)
+            # return HttpResponse('Login Done')
+            # request.session['message'] = 'Login Successful!!'
             return HttpResponseRedirect(reverse('profile'))
         else:
-            return  HttpResponse('Username or password is invalid',status=401)
+            return HttpResponse('Username or password is invalid', status=401)
     else:
-        return HttpResponse("Invalid Request Type",status=400)
+        return HttpResponse("Invalid Request Type", status=400)
 
 
 def logout_user(request):
-    if request.method=='GET':
+    if request.method == 'GET':
         if request.user.is_authenticated:
             logout(request)
             return HttpResponse('Logged Out Successfully')
         return HttpResponse('Already logged out')
 
-    return HttpResponse("Invalid Request Type",status=400)
+    return HttpResponse("Invalid Request Type", status=400)
 
 
 def view_login_user(request):
